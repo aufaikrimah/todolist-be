552126b85d3b6f74e597ef56644f68d2c3be988a
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-13 16:41:10 +0700

    add comment for routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const todoController = require('../controllers/todoController');
 
+//endpoint auth
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 
+//endpoint todos
 router.post('/new-todos', authController.authenticateUser, todoController.createTodo);
 router.get('/all-todos', authController.authenticateUser, todoController.getAllTodos);
 router.get('/todo/:id', authController.authenticateUser, todoController.getDetailTodo);
