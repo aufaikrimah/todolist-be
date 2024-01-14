5551a4fba1f7e8b0b323e15f3c8ecc0e808f3d89
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-15 00:04:58 +0700

    change config.js

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const config = {
+  development: {
+    username: "root",
+    password: "Rahmawati-1.",
+    database: "todolist",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || "database_production",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "mysql"
+  }
+};
+
+module.exports = config;
