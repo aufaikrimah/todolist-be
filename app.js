8ed087ee33ec5ea09541ffeb73c811f63dabc188
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-14 23:49:19 +0700

    add config for environment variables in railway

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 const dotenv = require('dotenv');
 const app = express();
-const PORT = process.env.PORT || 3000;
 dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- models/index.js
+++ models/index.js
@@ -3,9 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+dotenv.config();
+
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
