9bda89421c02086bff0b9c2f6ef54b324bf16233
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-15 09:34:23 +0700

    update connection db

--- models/index.js
+++ models/index.js
@@ -16,7 +16,11 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  });
 }
 
 fs
